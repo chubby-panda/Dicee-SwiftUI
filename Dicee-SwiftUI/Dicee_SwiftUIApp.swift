75250ab817a7c6dbb1ea012ce5f4e445ebb58302
Sophie Phoon <sophie.j.phoon@gmail.com>
2021-07-06 13:42:26 +0800

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Sophie Phoon on 6/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
