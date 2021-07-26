fb1ba42915738f40f631a0073d009977559b8e82
Sophie Phoon <sophie.j.phoon@gmail.com>
2021-07-26 17:15:05 +0800

    wip

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // use State to recreate the structure whenever the variable updates or changes
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+                
+                Button(action: {
+                    // what should happen when the button is pressed
+                    leftDiceNumber = Int.random(in: 1...6)
+                    rightDiceNumber = Int.random(in: 1...6)
+                }, label: {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                })
+                .background(Color(.red))
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 100, height: 100, alignment: .center)
             .padding()
     }
 }
@@ -19,3 +65,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
